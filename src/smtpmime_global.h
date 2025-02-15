ed378bd500a6443e4d5fc87a21f7cf216767d54d
David C. Partridge <david.partridge@perdrix.co.uk>
2025-02-15 04:49:32 +0000

    Avoid redefinition warning on MacOS

--- src/smtpmime_global.h
+++ src/smtpmime_global.h
@@ -1,13 +1,14 @@
 #ifndef SMTPMIME_GLOBAL_H
 #define SMTPMIME_GLOBAL_H
 
-#define SMTP_MIME_EXPORT
 #if defined(BUILD_SHARED_LIBS)
 #  ifdef SMTP_MIME_LIBRARY
 #    define SMTP_MIME_EXPORT Q_DECL_EXPORT
 #  else
 #    define SMTP_MIME_EXPORT Q_DECL_IMPORT
 #  endif
+#else
+#  define SMTP_MIME_EXPORT
 #endif
 
 #endif // SMTPMIME_GLOBAL_H
