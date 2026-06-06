863843c34e03b50d41b8ae8ab6af00e9351693ca
Attila Tőkés <attitokes@gmail.com>
2026-06-06 07:55:50 +0300

    fix integer overflow (#188)

--- src/mimebase64formatter.cpp
+++ src/mimebase64formatter.cpp
@@ -4,8 +4,8 @@ MimeBase64Formatter::MimeBase64Formatter(QIODevice *out) :
     MimeContentFormatter(out) {}
 
 qint64 MimeBase64Formatter::writeData(const char *data, qint64 maxLength) {
-    int lines = (maxLength - 1) / lineLength + 1;
-    for (int i = 1; i < lines; ++i) {
+    auto lines = (maxLength - 1) / lineLength + 1;
+    for (auto i = 1; i < lines; ++i) {
         output->write(data, lineLength);
         output->write("\r\n");
         data += lineLength;
